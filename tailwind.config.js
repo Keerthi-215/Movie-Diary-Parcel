26f8688ac78a4c62c347901e1514e9ffab1a6991
cvemir369 <cvemir369@gmail.com>
2024-12-13 09:12:29 +0100

    parcel set movie diary files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Include any files that you want to apply the styles to
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
